8c0a0a2db39f64455990a1eb96b80d5464a05f51
ASaparbek <TELIDAS>
2022-02-21 21:04:28 +0600

    fifth lesson

--- shows/urls.py
+++ shows/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from . import views
+from . import views, models
 
 app_name = "shows"
 urlpatterns = [
-    path("shows/", views.get_shows_all, name="shows_list"),
-    path("shows/<int:id>/", views.get_show_detail, name="shows_detail"),
-    path("shows/<int:id>/update/", views.put_shows_update, name="shows_update"),
-    path("shows/<int:id>/delete/", views.shows_delete, name="shows_delete"),
-    path("add-shows/", views.add_show, name="add_show"),
+    path("shows/", views.ShowsListView.as_view(), name="shows_list"),
+    path("shows/anime/", views.ShowsListView.as_view(queryset=models.TVShow.objects.filter(genre="Anime").order_by("-created_date")), name="shows_anime_list"),
+    path("shows/action/", views.ShowsListView.as_view(queryset=models.TVShow.objects.filter(genre="Action").order_by("-created_date")), name="shows_action_list"),
+    path("shows/drama/", views.ShowsListView.as_view(queryset=models.TVShow.objects.filter(genre="Drama").order_by("-created_date")), name="shows_drama_list"),
+    path("shows/<int:id>/", views.ShowsDetailView.as_view(),
+         name="shows_detail"),
+    path("shows/<int:id>/update/", views.ShowsUpdateView.as_view(),
+         name="shows_update"),
+    path("shows/<int:id>/delete/", views.ShowsDeleteView.as_view(),
+         name="shows_delete"),
+    path("add-shows/", views.ShowsCreateView.as_view(),
+         name="add_show"),
 ]
--- shows/views.py
+++ shows/views.py
@@ -3,55 +3,106 @@ from . import models, forms
 from django.shortcuts import get_object_or_404
 from django.shortcuts import reverse, redirect
 from django.http import Http404, HttpResponse
+from django.views import generic
 
 
-def get_shows_all(request):
-    shows = models.TVShow.objects.filter().order_by("-id")
-    return render(request, "shows_list.html", {"shows": shows})
+class ShowsListView(generic.ListView):
+    template_name = "shows_list.html"
+    queryset = models.TVShow.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
 
 
-def get_show_detail(request, id):
-    global comment
-    try:
-        show = get_object_or_404(models.TVShow, id=id)
-        try:
-            comment = models.ShowComment.objects.filter(shows_id=id).order_by("created_date")
-        except models.TVShow.DoesNotExist:
-            print('No comments')
-    except models.TVShow.DoesNotExist:
-        raise Http404('TVSHOW does not exist, try another id')
-    return render(request, "shows_detail.html", {"show": show, 'shows_comment': comment})
+# def get_shows_all(request):
+#     shows = models.TVShow.objects.filter().order_by("-id")
+#     return render(request, "shows_list.html", {"shows": shows})
 
 
-def add_show(request):
-    method = request.method
-    if method == "POST":
-        form = forms.TVShowForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse("shows:shows_list"))
-            # return HttpResponse("Show Created Successfully")
-    else:
-        form = forms.TVShowForm()
-    return render(request, "add_shows.html", {"form": form})
+class ShowsDetailView(generic.DetailView):
+    template_name = "shows_detail.html"
+
+    def get_object(self, **kwargs):
+        shows_id = self.kwargs.get("id")
+        return get_object_or_404(models.TVShow, id=shows_id)
 
 
-def put_shows_update(request, id):
-    show_id = get_object_or_404(models.TVShow, id=id)
-    if request.method == "POST":
-        form = forms.TVShowForm(instance=show_id,
-                                data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse("shows:shows_list"))
-    else:
-        form = forms.TVShowForm(instance=show_id)
-    return render(request, "shows_update.html", {"form": form,
-                                                 "show": show_id})
+# def get_show_detail(request, id):
+#     global comment
+#     try:
+#         show = get_object_or_404(models.TVShow, id=id)
+#         try:
+#             comment = models.ShowComment.objects.filter(shows_id=id).order_by("created_date")
+#         except models.TVShow.DoesNotExist:
+#             print('No comments')
+#     except models.TVShow.DoesNotExist:
+#         raise Http404('TVSHOW does not exist, try another id')
+#     return render(request, "shows_detail.html", {"show": show, 'shows_comment': comment})
 
 
-def shows_delete(request, id):
-    show_id = get_object_or_404(models.TVShow, id=id)
-    show_id.delete()
-    # return HttpResponse("Show Deleted")
-    return redirect(reverse("shows:shows_list"))
+class ShowsCreateView(generic.CreateView):
+    template_name = "add_shows.html"
+    form_class = forms.TVShowForm
+    queryset = models.TVShow.objects.all()
+    success_url = "/shows/"
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(ShowsCreateView, self).form_valid(form=form)
+
+
+# def add_show(request):
+#     method = request.method
+#     if method == "POST":
+#         form = forms.TVShowForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse("shows:shows_list"))
+#             # return HttpResponse("Show Created Successfully")
+#     else:
+#         form = forms.TVShowForm()
+#     return render(request, "add_shows.html", {"form": form})
+
+
+class ShowsUpdateView(generic.UpdateView):
+    template_name = "shows_update.html"
+    form_class = forms.TVShowForm
+    success_url = "/shows/"
+
+    def get_object(self, **kwargs):
+        shows_id = self.kwargs.get("id")
+        return get_object_or_404(models.TVShow, id=shows_id)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(ShowsUpdateView, self).form_valid(form=form)
+
+
+# def put_shows_update(request, id):
+#     show_id = get_object_or_404(models.TVShow, id=id)
+#     if request.method == "POST":
+#         form = forms.TVShowForm(instance=show_id,
+#                                 data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse("shows:shows_list"))
+#     else:
+#         form = forms.TVShowForm(instance=show_id)
+#     return render(request, "shows_update.html", {"form": form,
+#                                                  "show": show_id})
+
+
+class ShowsDeleteView(generic.DeleteView):
+    success_url = "/shows/"
+    template_name = "confirm_delete_show.html"
+
+    def get_object(self, **kwargs):
+        shows_id = self.kwargs.get("id")
+        return get_object_or_404(models.TVShow, id=shows_id)
+
+
+# def shows_delete(request, id):
+#     show_id = get_object_or_404(models.TVShow, id=id)
+#     show_id.delete()
+#     # return HttpResponse("Show Deleted")
+#     return redirect(reverse("shows:shows_list"))
